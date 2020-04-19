5ddcc6ee84d8ba26aa080cc3c65b5d19f378d9e0
JelleAalbers <jelleaalbers@gmail.com>
2020-04-19 09:35:40 -0500

    Re-source environment when starting shell

--- _xentenv_inner.sh
+++ _xentenv_inner.sh
@@ -18,6 +18,8 @@ if [[ $1 = "-j" ]]; then
     echo "Starting jupyter server on host $JUP_HOST, port $JUP_PORT"
     jupyter lab --config /project2/lgrandi/xenonnt/development/jupyter_notebook_config.py --no-browser --port=$JUP_PORT --ip=$JUP_HOST 2>&1
 else
-    export PS1="[nT \u@\h \W]\$ "
-    bash
+    echo "Starting shell in the XENONnT environment"
+    echo "In order to get everything right, we seem to have to source the env another time"
+    echo "This can give warnings since the env has sometimes sourced itself already"
+    bash --rcfile <(echo '. ~/.bashrc; source /opt/XENONnT/setup.sh')
 fi
