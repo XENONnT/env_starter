0deeaee4b098d2a5813503db49e634f735dcd6d3
Peter Gaemers <petergaemers@hotmail.com>
2020-04-24 09:44:31 +0200

    Update start_jupyter.py
    For osx catalina open target requires the target to be surrounded by "". This pull request adds this to the printed urls

--- start_jupyter.py
+++ start_jupyter.py
@@ -77,6 +77,10 @@ If you have a mac, instead do:
 
 ssh -fN -L {port}:{ip}:{port} {username}@dali-login2.rcc.uchicago.edu && open http://localhost:{port}/{token}
 
+If you have a mac with os Catalina, instead do:
+
+ssh -fN -L {port}:{ip}:{port} {username}@dali-login2.rcc.uchicago.edu && open "http://localhost:{port}/{token}"
+
 Happy strax analysis, {username}!
 """
 
