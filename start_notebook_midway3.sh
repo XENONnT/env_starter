3c6e5034aba4203bd016a895005aba44e1b46010
Lanqing Yuan <yuanlq@uchicago.edu>
2023-05-08 12:02:21 -0500

    trial on redirecting home

--- start_notebook_midway3.sh
+++ start_notebook_midway3.sh
@@ -55,4 +55,4 @@ EOF
 chmod +x $INNER
 
 module load singularity
-singularity exec --bind /project2 --bind /scratch/midway3/$USER --bind /scratch/midway2/$USER --bind /project/lgrandi --bind /project2/lgrandi/xenonnt/dali:/dali $CONTAINER $DIR/$INNER
+singularity exec --home /gpfs/midway2/perf2/home/$USER --bind /project2 --bind /scratch/midway3/$USER --bind /scratch/midway2/$USER --bind /project/lgrandi --bind /project2/lgrandi/xenonnt/dali:/dali $CONTAINER $DIR/$INNER
