b6544f79484c0a67d2284a665eba7ac91e527e4e
Lanqing Yuan <yuanlq@uchicago.edu>
2023-08-30 21:04:04 -0500

    debug info

--- start_jupyter.py
+++ start_jupyter.py
@@ -303,7 +303,8 @@ def main():
 
         _want_to_make_reservation = ((args.partition == 'xenon1t' or args.partition == 'lgrandi')
                                      and (not args.bypass_reservation))
-        print_flush("You are using partition %s and you want to use reservation" % args.partition)
+        print_flush("You are using partition '%s' and you want to use reservation named '%s'"%(args.partition, 
+                                                                                               reservation_name))
         if args.ram > 16000 and _want_to_make_reservation:
             print_flush('You asked for more than 16 GB total memory you cannot use the notebook '
                         'reservation queue for this job! We will bypass the reservation.')
@@ -317,6 +318,7 @@ def main():
                 and args.cpu < 8
                 and args.ram <= 16000
         )
+        print_flush("Are you using reservation? %s" % use_reservation)
 
         job_fn = os.path.join(OUTPUT_DIR[args.partition], f'notebook{unique_id}.sbatch')
         if not args.force_new:
