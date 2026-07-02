40cbdd2b34031952c4e9ab34aa054ee340e309c4
dachengx <dx2227@columbia.edu>
2026-07-02 06:02:58 -0500

    More success messages for Windows powershell

--- start_jupyter.py
+++ start_jupyter.py
@@ -144,6 +144,10 @@ All done! If you have linux, execute this command on your laptop:
 
 ssh -fN -L {port}:{ip}:{port} {username}@{hostname} && sensible-browser http://localhost:{port}/{token}
 
+If you have a windows powershell, instead do (open browser manually if it doesn't prompt):
+
+ssh -N -L {port}:{ip}:{port} {username}@dali-login2.rcc.uchicago.edu; Start-Process "http://localhost:{port}/{token}"
+
 If you have a mac, instead do:
 
 ssh -fN -L {port}:{ip}:{port} {username}@{hostname} && open "http://localhost:{port}/{token}"
